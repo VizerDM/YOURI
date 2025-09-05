c7d5a778972a44d51240013526809ee7884b6d08
VizerDM <simo.elyou105@gmail.com>
2025-09-05 19:55:48 +0100

    "Added Image in ProdCard"

--- YOURI_WEB/src/Components/ProductCard.tsx
+++ YOURI_WEB/src/Components/ProductCard.tsx
@@ -1,11 +1,13 @@
 interface Props {
   Title: string;
+  Image: string;
 }
 
-function ProductCard({ Title }: Props) {
+function ProductCard({ Title, Image }: Props) {
   return (
     <>
       <h1>{Title}</h1>
+      <img src={Image} alt="an image"></img>
       <div>ProductCard</div>
     </>
   );
--- YOURI_WEB/src/HomePage.tsx
+++ YOURI_WEB/src/HomePage.tsx
@@ -1,11 +1,12 @@
 //Components
 import ProductCard from "./Components/ProductCard";
+import JacketMock from "./assets/JacketMockUp.jpeg";
 
 //Main Function
 function HomePage() {
   return (
     <>
-      <ProductCard Title="Latest Products" />
+      <ProductCard Title="Latest Products" Image={JacketMock} />
     </>
   );
 }
